1eb2628a618c95e78445b80389a595e4d6925c09
javenkn <javenkn7@gmail.com>
2023-03-24 18:42:18 -1000

    Adds tests for generic Question enum

--- QuizApp/NavigationControllerRouter.swift
+++ QuizApp/NavigationControllerRouter.swift
@@ -21,7 +21,7 @@ class NavigationControllerRouter: Router {
         self.factory = factory
     }
     
-    func routeTo(question: Question, answerCallback: @escaping (String) -> Void) {
+    func routeTo(question: String, answerCallback: @escaping (String) -> Void) {
         let viewController = factory.questionViewController(for: question, answerCallback: answerCallback)
         navigationController.pushViewController(viewController, animated: true)
     }
---
+++ QuizApp/Question.swift
@@ -0,0 +1,33 @@
+//
+//  Question.swift
+//  QuizApp
+//
+//  Created by J on 3/24/23.
+//
+
+import Foundation
+
+enum Question<T: Hashable> : Hashable {
+    case singleAnswer(T)
+    case multipleAnswer(T)
+    
+    func hash(into hasher: inout Hasher) {
+        switch self {
+        case .singleAnswer(let a):
+          hasher.combine(a)
+        case .multipleAnswer(let a):
+          hasher.combine(a)
+        }
+      }
+    
+    static func ==(lhs: Question<T>, rhs: Question<T>) -> Bool {
+        switch (lhs, rhs) {
+        case (.singleAnswer(let a), .singleAnswer(let b)):
+            return a == b
+        case (.multipleAnswer(let a), .multipleAnswer(let b)):
+            return a == b
+        default:
+            return false
+        }
+    }
+}
